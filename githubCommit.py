8d8739c34db45ae75439911b47037c670129ec78
alex-eliot <82461605+alex-eliot@users.noreply.github.com>
2021-05-20 21:42:56 +0300

    given option to choose whether to create git.io link

--- githubCommit.py
+++ githubCommit.py
@@ -16,15 +16,16 @@ def makeFileAndGetGist(filedata, friendlyName):
 
     raw_url = "https://raw.githubusercontent.com/{}/main/{}".format(data["repoDirectory"], friendlyName)
 
-    customName = input("Input a custom name for the cubari link (leave empty to create a random one): ")
-    if customName == "":
-        r = requests.post(url, data={"url": raw_url})
-    else:
-        r = requests.post(url, data={"url": raw_url, "code": customName})
-    if r.status_code == 200:
-        return "https://cubari.moe/read/gist/{}/".format(r.text)
-    else:
-        return None
+    if input("Would you like to create a git.io link? (y/n): ") == "y":
+        customName = input("Input a custom name for the cubari link (leave empty to create a random one): ")
+        if customName == "":
+            r = requests.post(url, data={"url": raw_url})
+        else:
+            r = requests.post(url, data={"url": raw_url, "code": customName})
+        if r.status_code == 200:
+            return "https://cubari.moe/read/gist/{}/".format(r.text)
+        else:
+            return None
 
 def self_start():
     with open("cubari.json", 'r') as f:
